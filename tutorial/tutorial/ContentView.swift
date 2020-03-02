157779ef51dc1b2a137af057457947b722a8630a
milk <milk@motherapp.com>
2020-03-02 13:22:51 +0800

    [feature] outlining UI

--- tutorial/tutorial/ContentView.swift
+++ tutorial/tutorial/ContentView.swift
@@ -9,13 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let redTarget = Double.random(in: 0..<1)
+    let greenTarget = Double.random(in: 0..<1)
+    let blueTarget = Double.random(in: 0..<1)
+    @State var redGuess: Double
+    @State var greenGuess: Double
+    @State var blueGuess: Double
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                // Target Color Block
+                VStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .foregroundColor(Color(red: redTarget, green: greenTarget, blue: blueTarget, opacity: 1.0))
+                    Text("Match this Color")
+                }
+                // Guess Color Block
+                VStack {
+                    RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(Color(red: redGuess, green: greenGuess, blue: blueGuess, opacity: 1.0))
+                    HStack {
+                        Text("Red")
+                        Text("Green")
+                        Text("Blue")
+                    }
+                }
+            }
+            
+            Image(systemName: "star.fill")
+                .imageScale(.large)
+                .foregroundColor(.red)
+            
+            VStack {
+                Text("Red")
+                Text("Green")
+                Text("Blue")
+            }
+        }
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(redGuess: 0.5, greenGuess: 0.5, blueGuess: 0.5 )
     }
 }
+#endif
--- tutorial/tutorial/SceneDelegate.swift
+++ tutorial/tutorial/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView(redGuess: 0.5, greenGuess: 0.5, blueGuess: 0.5 )
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
