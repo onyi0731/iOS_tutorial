4274dc1cbabe1332287542a3171469998413d8eb
milk <milk@motherapp.com>
2020-03-02 11:55:04 +0800

    [feature] init project

---
+++ tutorial/tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by On Yi Wong on 2/3/2020.
+//  Copyright © 2020 On Yi Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
